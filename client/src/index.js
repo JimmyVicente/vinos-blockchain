deee338edb30fb786ab1596f28832a5f95ee25ec
JimmyVicente <jimmyvicentel@gmail.com>
2022-06-14 12:08:50 -0500

    vista base de cliente

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Sidebar from './components/Sidebar'
+import { Flex, Text, IconButton } from '@chakra-ui/react'
+import { FiMenu } from 'react-icons/fi'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default function Home() {
+  return (
+    <Flex w="100%">
+      <Sidebar />
+      <Flex
+        pos="absolute"
+        top="50%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+      >
+        <Text>Click the
+          <IconButton
+            background="none"
+            _hover={{ background: 'none' }}
+            icon={<FiMenu />}
+          />
+        to resize the vertical navigation bar.</Text>
+      </Flex>
+    </Flex>
+  )
+}
